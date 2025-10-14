38a98eb68f9803b4a9c6fcfd20203c199774d745
Tejas <tejasspatell2@gmail.com>
2025-10-15 00:05:28 +0530

    made frontend

--- Frontend/src/components/ForgotPassword.jsx
+++ Frontend/src/components/ForgotPassword.jsx
@@ -1,35 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../style/ForgotPassword.css";
 
 export default function ForgotPassword() {
+    const [otpStage, setotpStage] = useState(false);
+    const [otp, setOtp] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setotpStage(true);
+    }
+
+    const handleOtpSubmit = (e) => {
+        e.preventDefault();
+        if (otp.length === 6) {
+            alert("OTP Verified! You can reset your password now.");
+        }
+        else {
+            alert("Enter a valid 6-digit OTP");
+        }
+    }
+
     return (
         <div className="auth-container">
             <form className="forgot-form">
-
-                <div className="forgot-container">
-                    <h1>Forgot Password</h1>
-                    <p className="forgot-desc">
-                        Enter your email below and we'll send you a link to reset your password.
-                    </p>
-                    <div className="forgot-div">
-                        <label htmlFor="email">
-                            Email
-                            <input
-                                id="email"
-                                type="email"
-                                placeholder="Enter your email"
-                                required
-                            />
-                        </label>
-                        <button type="submit" className="forgot-btn">
-                            Send Reset Link
-                        </button>
-                    </div>
-                    <p className="forgot-text">
-                        Remember your password? <Link to="/login">Login</Link>
-                    </p>
-                </div>
+                {otpStage ? (
+                    <>
+                        <h1>Forgot Password</h1>
+                        <p className="forgot-desc">
+                            Enter your email below and we'll send you a link to reset your password.
+                        </p>
+                        <div className="forgot-div">
+                            <label htmlFor="email">
+                                Email
+                                <input
+                                    id="email"
+                                    type="email"
+                                    placeholder="Enter your email"
+                                    required
+                                />
+                            </label>
+                            <button onClick={handleSubmit} type="submit" className="forgot-btn">
+                                Send Reset Link
+                            </button>
+                        </div>
+                        <p className="forgot-text">
+                            Remember your password? <Link to="/login">Login</Link>
+                        </p>
+                    </>
+                ) : (
+                    <>
+                        <h1>Enter OTP</h1>
+                        <p className="forgot-desc">
+                            We've sent a 6-digit code to your email.
+                        </p>
+                        <div className="forgot-div">         
+                                <input
+                                    id="otp"
+                                    type="text"
+                                    maxLength={6}
+                                    value={otp}
+                                    onChange={(e) => setOtp(e.target.value.replace(/\D/, ""))}
+                                    placeholder="Enter 6-digit code"
+                                    required
+                                />                           
+                            <button
+                                type="submit"
+                                className="forgot-btn"
+                                onClick={handleOtpSubmit}>
+                                Verify OTP
+                            </button>
+                        </div>
+                    </>
+                )}
             </form>
         </div>
     );
