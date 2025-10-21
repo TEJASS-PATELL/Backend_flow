7844d202edc47095e3ed2973ba9c85d64cfcc32c
Tejas <tejasspatell2@gmail.com>
2025-10-21 23:43:25 +0530

    build backend

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,      //! add your cloudinary variables name in .env file 
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,25 +1,43 @@
+import cloudinary from "../config/cloudinary.js";
 import prisma from "../config/prisma.js";
 
 export const UserData = async (req, res) => {
-    try{
-        const {userId} = req.body;
+    try {
+        const { userId } = req.body;
 
-        const user = await prisma.user.findUnique({where: {id: userId}});
-        if(!user){
+        const user = await prisma.user.findUnique({ where: { id: userId } });
+        if (!user) {
             return res.status().json();
         }
 
-        res.json({success: true, userData : {
-            name: user.name,
-            isAccountVerified: user.isAccountVerified,
-            email: user.email
-        }})
+        res.json({
+            success: true, userData: {
+                name: user.name,
+                isAccountVerified: user.isAccountVerified,
+                email: user.email
+            }
+        })
 
-    }catch(err){
-        res.status(500).json({success: false, message: "Error occur in get User data function"});
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Error occur in get User data function" });
     }
 }
 
 export const UserProfile = async (req, res) => {
-    
+    try {
+        const { userId, profilePic } = req.body;
+
+        if (!userId) return res.status(401).json({ message: "User ID is missing" });
+        if (!profilePic) return res.status(400).json({ message: "Profile picture is required" });
+
+        const uploadProfilePic = cloudinary.uploader.upload(profilePic, { folder: "users_profile" });
+
+        await prisma.user.create({ where: { id: userId }, data: { profilePic: (await uploadProfilePic).secure_url } });
+
+        return res.status(200).json({ success: true, message: "User Profile updated succesfully" });
+    }
+    catch (error) {
+        console.error("Error in updateProfile:", error.stack);
+        res.status(500).json({ success: false, message: "Error while uploading profile" });
+    }
 }
