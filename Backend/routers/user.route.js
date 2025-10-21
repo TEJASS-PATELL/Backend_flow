a700c45eedbbdefd2395170c05c60c3df2f26d25
Tejas <tejasspatell2@gmail.com>
2025-10-21 23:01:07 +0530

    build backend

--- Backend/routers/user.route.js
+++ Backend/routers/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import authMiddleware from "../middlewares/auth.middleware";
-import { UserData } from "../controllers/user.controller";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import { UserData } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/data", authMiddleware, UserData);
+
+export default router;
