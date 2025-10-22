47e8f55f0851f314bbc4a69c20cff24cc2908622
Tejas <tejasspatell2@gmail.com>
2025-10-22 23:17:08 +0530

    build frontned

--- Frontend/src/layout/NavBar.jsx
+++ Frontend/src/layout/NavBar.jsx
@@ -5,9 +5,10 @@ import "../style/Layout.css";
 export default function NavBar() {
   return (
     <nav className="navbar">
-      <div className="nav-logo">MyApp</div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Review</Link></li>
+        <li><Link to="/">Faq's</Link></li>
         <li><Link to="/signup">Sign Up</Link></li>
       </ul>
     </nav>
